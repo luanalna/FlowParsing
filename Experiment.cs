1a9d842a7ecde44ff6ef3f76cfe91ba2b1c36246
Luana Aldea <luana.aldea@gmail.com>
2024-05-15 00:06:26 +0200

    second commit

--- Experiment.cs
+++ Experiment.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using System.Linq;
 using System.IO;
 
+// wtf
 public class ExperimentGenerator : MonoBehaviour
 {
     // Canvas elements for different phases of the experiment
